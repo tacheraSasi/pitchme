4e5428a33c7392d02b1b47e4db24415145c3de21
Tachera Sasi <tacherasasi@gmail.com>
2025-10-29 08:46:57 +0300

    refactor: update getNotePath to use require for audio source retrieval

--- constants/notes.ts
+++ constants/notes.ts
@@ -20,3 +20,18 @@ export function getNote(note:Note){
         (n)=>n.toLowerCase() === note.toLowerCase()
     )
 }
+
+const noteAssets: Record<Note, any> = {
+  [Note.C]: require("../assets/notes/C.m4a"),
+  [Note.CSharp]: require("../assets/notes/CSharp.m4a"),
+  [Note.D]: require("../assets/notes/D.m4a"),
+  [Note.DSharp]: require("../assets/notes/DSharp.m4a"),
+  [Note.E]: require("../assets/notes/E.m4a"),
+  [Note.F]: require("../assets/notes/F.m4a"),
+  [Note.FSharp]: require("../assets/notes/FSharp.m4a"),
+  [Note.G]: require("../assets/notes/G.m4a"),
+  [Note.GSharp]: require("../assets/notes/GSharp.m4a"),
+  [Note.A]: require("../assets/notes/A.m4a"),
+  [Note.ASharp]: require("../assets/notes/ASharp.m4a"),
+  [Note.B]: require("../assets/notes/B.m4a"),
+};
--- utils/notes.ts
+++ utils/notes.ts
@@ -3,6 +3,6 @@ import { getNote, Note } from "@/constants/notes";
 export function getNotePath(note: Note) {
     const foundNote = getNote(note);
     if (!foundNote) return null;
-
-    return `../assets/notes/${foundNote}.m4a`;
+    const audioSource = require(`../assets/notes/${foundNote}.m4a`);
+    return audioSource;
 }
