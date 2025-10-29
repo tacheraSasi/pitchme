6140a3df85f3fd0d6f818abd44aa9379ce17dc0d
Tachera Sasi <tacherasasi@gmail.com>
2025-10-29 17:33:41 +0300

    add tab header component

---
+++ components/tabs-header.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TabsHeader = () => {
+  return (
+    <View>
+      <Text>TabsHeader</Text>
+    </View>
+  )
+}
+
+export default TabsHeader
