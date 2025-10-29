81c650ea904bef84b3f826b26412a9b627d883ab
Tachera Sasi <tacherasasi@gmail.com>
2025-10-29 09:27:41 +0300

    refactor: update header image and remove unused components in TabTwoScreen; adjust icon mapping in IconSymbol

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -18,7 +18,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
-} as IconMapping;
+  'mic.circle.fill': 'mic',
+} as Partial<IconMapping>;
 
 /**
  * An icon component that uses native SF Symbols on iOS, and Material Icons on Android and web.
