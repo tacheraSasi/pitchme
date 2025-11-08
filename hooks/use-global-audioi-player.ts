43c53a6e96c2dc19b92798e1e752ea34c1d28157
Tachera Sasi <tacherasasi@gmail.com>
2025-11-08 23:42:41 +0300

    Update use-global-audioi-player.ts

--- hooks/use-global-audioi-player.ts
+++ hooks/use-global-audioi-player.ts
@@ -1,8 +1,14 @@
 import { useAudioPlayer } from "expo-audio";
+import * as FileSystem from "expo-file-system";
 
 let currentPlayer: any = null;
 
 export function useGlobalAudioPlayer(uri: string) {
+  const audioFile = new FileSystem.File(uri);
+  if (!audioFile) throw new Error("Invalid audio file"); //TODO: i will handle such errors better
+
+  if (!audioFile.exists) throw new Error("Audio file does not exist");
+
   const player = useAudioPlayer({ uri });
 
   const playExclusive = async () => {
