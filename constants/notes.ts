c5e9497628f0c5137200bac1835879a8550566ae
tacheraSasi <tacherasasi@gmail.com>
2025-12-03 22:01:05 +0300

    Refactor code structure for improved readability and maintainability

--- constants/notes.ts
+++ constants/notes.ts
@@ -18,18 +18,18 @@ export const NOTES: Note[] = Object.values(Note);
 export function getNote(note: Note) {
   return NOTES.find((n) => n.toLowerCase() === note.toLowerCase());
 }
-
+const defaultPath = "../assets/notes/tach/";
 export const noteAssets: Record<Note, any> = {
-  [Note.C]: require("../assets/notes/C.m4a"),
-  [Note.CSharp]: require("../assets/notes/CSharp.m4a"),
-  [Note.D]: require("../assets/notes/D.m4a"),
-  [Note.DSharp]: require("../assets/notes/DSharp.m4a"),
-  [Note.E]: require("../assets/notes/E.m4a"),
-  [Note.F]: require("../assets/notes/F.m4a"),
-  [Note.FSharp]: require("../assets/notes/FSharp.m4a"),
-  [Note.G]: require("../assets/notes/G.m4a"),
-  [Note.GSharp]: require("../assets/notes/GSharp.m4a"),
-  [Note.A]: require("../assets/notes/A.m4a"),
-  [Note.ASharp]: require("../assets/notes/ASharp.m4a"),
-  [Note.B]: require("../assets/notes/B.m4a"),
+  [Note.C]: require(defaultPath+"/C.m4a"),
+  [Note.CSharp]: require(defaultPath+"/CSharp.m4a"),
+  [Note.D]: require(defaultPath+"/D.m4a"),
+  [Note.DSharp]: require(defaultPath+"/DSharp.m4a"),
+  [Note.E]: require(defaultPath+"/E.m4a"),
+  [Note.F]: require(defaultPath+"/F.m4a"),
+  [Note.FSharp]: require(defaultPath+"/FSharp.m4a"),
+  [Note.G]: require(defaultPath+"/G.m4a"),
+  [Note.GSharp]: require(defaultPath+"/GSharp.m4a"),
+  [Note.A]: require(defaultPath+"/A.m4a"),
+  [Note.ASharp]: require(defaultPath+"/ASharp.m4a"),
+  [Note.B]: require(defaultPath+"/B.m4a"),
 };
