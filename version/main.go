680a9ed3121649790ac598d2a1d0285435e989a8
tacheraSasi <tacherasasi@gmail.com>
2026-01-13 18:57:48 +0300

    Read and display app.json content in bump-version utility

--- version/main.go
+++ version/main.go
@@ -5,7 +5,19 @@ import (
 	"os"
 )
 
+const APP_JSON_PATH = "../app.json"
+
 func main(){
+	appJson, err := os.ReadFile(APP_JSON_PATH)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	appJsonData := string(appJson)
+
+	fmt.Println("app.json opened successfully")
+	fmt.Println("File path:", APP_JSON_PATH)
+	fmt.Println("File data", appJsonData)
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: bump-version <version>")
 		os.Exit(1)
