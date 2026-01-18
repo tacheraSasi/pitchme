6c0f3038e0ea1cdb92e8f944ca7b39f2f89fcc12
tacheraSasi <tacherasasi@gmail.com>
2026-01-18 16:23:05 +0300

    Bump app version to 1.0.8 and update privacy URL

--- constants/external-urls.ts
+++ constants/external-urls.ts
@@ -1,2 +1,2 @@
-export const privacyPolicyUrl = "https://www.notion.so/Privacy-Policy-for-PitchMe-2d214324f4a0800c8717fc39ea9981ae"
+export const privacyPolicyUrl = "https://clean-notify-3d7.notion.site/Privacy-Policy-for-PitchMe-2d214324f4a0800c8717fc39ea9981ae?source=copy_link"
 export const supportUrl = "mailto:support@ekilie.com"
